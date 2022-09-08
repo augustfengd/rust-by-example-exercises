1e50b4c8042a0ef9de4a81a40ad8c5a002d66806
August Feng <au.fengster@gmail.com>
2022-09-07 22:28:49 -0400

    modify

--- 4-variable-bindings/4-freezing/one.rs
+++ 4-variable-bindings/4-freezing/one.rs
@@ -5,9 +5,7 @@ fn main() {
         // Shadowing by immutable `_mutable_integer`
         let _mutable_integer = _mutable_integer;
 
-        // Error! `_mutable_integer` is frozen in this scope
-        _mutable_integer = 50;
-        // FIXME ^ Comment out this line
+        // _mutable_integer = 50;
 
         // `_mutable_integer` goes out of scope
     }
