afb221c9179287282e51e21b4c6d5fa153e88bc7
August Feng <au.fengster@gmail.com>
2022-09-07 22:59:19 -0400

    modify

--- 5-types/3-inference/one.rs
+++ 5-types/3-inference/one.rs
@@ -8,7 +8,7 @@ fn main() {
     // just knows that it's a vector of something (`Vec<_>`).
 
     // Insert `elem` in the vector.
-    vec.push(elem);
+    // vec.push(elem);
     // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
     // TODO ^ Try commenting out the `vec.push(elem)` line
 
