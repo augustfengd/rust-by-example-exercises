c25eaf3813dfbe9a4aee3d8ad4c44309565d71e2
August Feng <au.fengster@gmail.com>
2022-09-08 20:11:05 -0400

    modify

--- 8-flow-of-control/5-match/2-guards/two.rs
+++ 8-flow-of-control/5-match/2-guards/two.rs
@@ -4,7 +4,7 @@ fn main() {
     match number {
         i if i == 0 => println!("Zero"),
         i if i > 0 => println!("Greater than zero"),
-        // _ => unreachable!("Should never happen."),
+        _ => unreachable!("Should never happen."),
         // TODO ^ uncomment to fix compilation
     }
 }
