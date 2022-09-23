8deaf6489dacae7e75331fcb7beadecd0ab29600
August Feng <au.fengster@gmail.com>
2022-09-22 20:02:24 -0400

    add

---
+++ 19-std-library-types/5-result/1-/learn-1.rs
@@ -0,0 +1,64 @@
+mod checked {
+    #[derive(Debug)]
+    pub enum MathError {
+        DivisionByZero,
+        NonPositiveLogarithm,
+        NegativeSquareRoot,
+    }
+
+    pub type MathResult = Result<f64, MathError>;
+
+    pub fn div(x: f64, y: f64) -> MathResult {
+        if y == 0.0 {
+            // This operation would `fail`, instead let's return the reason of
+            // the failure wrapped in `Err`
+            Err(MathError::DivisionByZero)
+        } else {
+            // This operation is valid, return the result wrapped in `Ok`
+            Ok(x / y)
+        }
+    }
+
+    pub fn sqrt(x: f64) -> MathResult {
+        if x < 0.0 {
+            Err(MathError::NegativeSquareRoot)
+        } else {
+            Ok(x.sqrt())
+        }
+    }
+
+    pub fn ln(x: f64) -> MathResult {
+        if x <= 0.0 {
+            Err(MathError::NonPositiveLogarithm)
+        } else {
+            Ok(x.ln())
+        }
+    }
+
+    fn op_(x: f64, y: f64) -> MathResult {
+        // if `div` "fails", then `DivisionByZero` will be `return`ed
+        let ratio = div(x, y)?;
+
+        let ln = ln(ratio)?;
+
+        sqrt(ln)
+    }
+
+    pub fn op(x: f64, y: f64) {
+        match op_(x, y) {
+            Err(why) => panic!("{}", match why {
+                MathError::NonPositiveLogarithm
+                    => "Logarithm of non-positive number",
+                MathError::DivisionByZero
+                    => "division by zero",
+                MathError::NegativeSquareRoot
+                    => "square root of negative number",
+            }),
+            Ok(sqrt) => println!("{}", sqrt),
+        }
+    }
+}
+
+fn main() {
+    checked::op(1.0, 10.0)
+}
