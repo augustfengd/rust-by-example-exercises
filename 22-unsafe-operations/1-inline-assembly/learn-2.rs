3269989388c7d58ca9a0a7777fb0ce87ed9f2755
August Feng <au.fengster@gmail.com>
2022-10-01 13:36:29 -0400

    add

---
+++ 22-unsafe-operations/1-inline-assembly/learn-2.rs
@@ -0,0 +1,15 @@
+fn main() {
+    use std::arch::asm;
+
+    let i: u64 = 3;
+    let o: u64;
+    unsafe {
+        asm!(
+            "mov {0}, {1}",
+            "add {0}, 5",
+            out(reg) o,
+            in(reg) i,
+        );
+    }
+    assert_eq!(o, 8);
+}
---
+++ 22-unsafe-operations/1-inline-assembly/learn-3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    use std::arch::asm;
+
+    let mut x: u64 = 3;
+    unsafe {
+        asm!("add {0}, 5", inout(reg) x);
+    }
+    assert_eq!(x, 8);
+}
